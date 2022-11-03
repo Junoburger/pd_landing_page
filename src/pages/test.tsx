db08fdb57c4fd1479ca77bf06164b3c8d7b0ed48
operatorultra <105389680+operatorultra@users.noreply.github.com>
2022-11-03 16:22:28 +0100

    Add color picker

---
+++ src/pages/test.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Test: NextPage = () => {
+	return <div>Mooi</div>;
+};
+
+export default Test;
---
+++ src/server/trpc/router/example.ts
@@ -0,0 +1,16 @@
+import { router, publicProcedure } from "../trpc";
+import { z } from "zod";
+
+export const exampleRouter = router({
+	hello: publicProcedure
+		.input(z.object({ text: z.string().nullish() }).nullish())
+		.query(({ input }) => {
+			return {
+				greeting: `Hello ${input?.text ?? "world"}`,
+				testArray: [input?.text],
+			};
+		}),
+	getAll: publicProcedure.query(({ ctx }) => {
+		return ctx.prisma.example.findMany();
+	}),
+});
